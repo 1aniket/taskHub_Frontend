efd432f5638024fc1432abf4cf63eb82aef168b1
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2025-11-14 12:47:39 +0530

    feat(auth) : implement sign-up page

--- app/routes/auth/sign-up.tsx
+++ app/routes/auth/sign-up.tsx
@@ -1,17 +1,152 @@
-import React from 'react'
-import type { Route } from './+types/layout';
+"use client";
 
+import React from "react";
+import type { Route } from "./+types/layout";
+import { useForm } from "react-hook-form";
+import { signInSchema, signUpSchema } from "@/lib/schema";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import {
+  FormControl,
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "TierUp | SignUp" },
-    { name: "Rise Together", content: "Welcome to TierUp , Now without further due lets create an account." },
+    { title: "TaskHub | Sign In" },
+    { name: "Rise Together", content: "Welcome to TierUp!" },
   ];
 }
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
 
-export default SignUp
+type signUpData = z.infer<typeof signUpSchema>;
+const SignUp = () => {
+  const form = useForm<signUpData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (values: signUpData) => {
+    console.log(values.email);
+  };
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <Card className="bg-muted gap-1 shadow-lg w-[90%] md:w-[40%] text-center rounded-xl">
+        <CardHeader className="text-2xl flex justify-center">
+          Welcome To{" "}
+          <span className="gradient-text font-bold flex">TaskHub</span>
+        </CardHeader>
+        <CardDescription className="mb-4">Create your account</CardDescription>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Monkey D Luffy"
+                        {...field}
+                        className="border-3"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="luffy@onepiece.com"
+                        {...field}
+                        className="border-3"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="*********"
+                        {...field}
+                        className="border-3"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="*********"
+                        {...field}
+                        className="border-3"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button className="bg-blue-600 px-3 py-6" type={"submit"}>
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex text-gray-600 justify-center mt-5 item-center">
+          Already have an account ? &nbsp;
+          <Link to="../signin" className="font-bold text-blue-600">
+            Sign In
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -5,3 +5,15 @@ export const signInSchema = z.object({
     email : z.email("Enter Valid Email !"),
     password : z.string().min(8 , "Minimum 8 Charachters required !")
 })
+
+export const signUpSchema = z.object({
+    email : z.email("Enter Valid Email !"),
+    password : z.string().min(8 , "Minimum 8 Charachters required !"),
+    name : z.string().min(3 , "Minimum 3 charachters Required !"),
+    confirmPassword : z.string().min(8 , "Minimum 8 charachters required !")
+}).refine(
+    (data)=> data.password === data.confirmPassword, {
+        path:["confirmPassword"],
+        message : "password do not match !"
+    }
+);
